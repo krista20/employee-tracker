08d01a5e0e1b3fee01ad5682499687eb74c635b5
Rush <kriscookies20@gmail.com>
2022-08-16 16:47:43 -0700

    added in employee table on the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,11 @@ CREATE TABLE role (
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES
     department(id) ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) UNIQUE NOT NULL,
+    last_name VARCHAR(30) UNIQUE NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    manager_id INT NULL
+);
