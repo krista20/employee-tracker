b6bf88a2dd3edd389c6dbbda0d49a8552d725210
Rush <kriscookies20@gmail.com>
2022-08-16 16:32:42 -0700

    added in seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,16 @@ CREATE DATABASE employee;
 USE employee;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES
+    department(id) ON DELETE CASCADE
 );
